9d4153b198cbfe2c73b6943c551b7f156f77ab89
Gumaro Melendez <cloudernew@gmail.com>
2025-01-07 13:35:46 -0700

    implement unit tests

--- test_ml.py
+++ test_ml.py
@@ -1,28 +1,56 @@
 import pytest
-# TODO: add necessary import
+import pandas as pd
+from sklearn.preprocessing import OneHotEncoder, LabelBinarizer
+from sklearn.ensemble import RandomForestClassifier
+from ml.data import apply_label, process_data
+from ml.model import train_model
 
-# TODO: implement the first test. Change the function name and input as needed
-def test_one():
+def test_train_model():
     """
-    # add description for the first test
+    Ensure that the function returns a model that is not None
     """
-    # Your code here
-    pass
+    X = pd.DataFrame({
+        "workclass": ["Private", "Public", "Private"],
+        "education": ["Bachelors", "Masters", "Bachelors"],
+        "salary": [0, 1, 0]
+    })
 
+    cat_features = ["workclass", "education"]
+    label = "salary"
+    X_train, y_train, encoder, lb = process_data(X, cat_features, label, training=True)
 
-# TODO: implement the second test. Change the function name and input as needed
-def test_two():
-    """
-    # add description for the second test
-    """
-    # Your code here
-    pass
+    model = train_model(X_train, y_train)
 
+    assert model is not None
+    assert isinstance(model, RandomForestClassifier)
 
-# TODO: implement the third test. Change the function name and input as needed
-def test_three():
+def test_process_data_training_true():
     """
-    # add description for the third test
+    Ensure that the function returns the correct values when training is True
     """
-    # Your code here
-    pass
+    X = pd.DataFrame({
+        "workclass": ["Private", "Public", "Private"],
+        "education": ["Bachelors", "Masters", "Bachelors"],
+        "salary": [0, 1, 0]
+    })
+
+    cat_features = ["workclass", "education"]
+    label = "salary"
+    X_train, y_train, encoder, lb = process_data(
+        X,
+        categorical_features=cat_features,
+        label=label,
+        training=True
+    )
+
+    assert X_train.shape == (3, 4)
+    assert y_train.shape == (3,)
+    assert isinstance(encoder, OneHotEncoder)
+    assert isinstance(lb, LabelBinarizer)
+
+def test_apply_label():
+    """
+    Ensure that the label 0 is converted to '<=50K' and 1 is converted to '>50K'
+    """
+    assert apply_label([0]) == "<=50K"
+    assert apply_label([1]) == ">50K"
